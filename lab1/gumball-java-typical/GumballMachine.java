4378f9789fdf326a2a8e0a431f4ea56d5a1b9b0d
PRANALI\DELL <pranalibhavsar139@gmail.com>
2018-09-06 18:03:32 -0700

    Problem 1  complete solution

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -3,41 +3,61 @@ public class GumballMachine
 {
 
     private int num_gumballs;
+    private int price;
     private boolean has_quarter;
+    private String machine;
+    private int sum;
+    private boolean has_coins;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size , String machine1, int price1)
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        this.machine = machine1;
+        this.price = price1;
+        this.has_coins = false;
+        this.sum = 0;
     }
 
-    public void insertQuarter(int coin)
+    public void insertCoin(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+        if (coin == 25){
+            this.sum += coin;
+            this.has_coins = true;
+        }
+        else if(machine.equals("All Coins")){
+             this.sum += coin;
+             this.has_coins = true;
+        }
+        else
+             System.out.println("Eject Coin. Only Quarter Accepted");
     }
-    
+           
     public void turnCrank()
     {
-    	if ( this.has_quarter )
-    	{
-    		if ( this.num_gumballs > 0 )
-    		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
-    		}
-    	}
-    	else 
-    	{
-    		System.out.println( "Please insert a quarter" ) ;
-    	}        
+        if (this.sum >= this.price )
+        {
+            if ( this.num_gumballs > 0 )
+            {
+                this.num_gumballs-- ;
+                this.has_coins = false;
+                this.sum -= this.price;
+                System.out.println( "Thanks for your payment.  Gumball Ejected from " + this.machine + "machine!" ) ;
+                if(this.sum >= this.price)
+                {
+                    this.has_coins = true;
+                    turnCrank();
+                }
+            }
+            else
+            {
+                System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+            }
+        }
+        else{
+            System.out.println("Insert more coins.");
+        }
+        
+        
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -2,21 +2,30 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5,"Quarter", 25);
+        GumballMachine gumballMachine1 = new GumballMachine(5,"All Coins", 50);
+        GumballMachine gumballMachine2 = new GumballMachine(5,"Double Quarter", 50);
 
-		System.out.println(gumballMachine);
+        gumballMachine.insertCoin( 25 );
+        gumballMachine.insertCoin( 25 );
+        gumballMachine.insertCoin( 25 );
+	gumballMachine.turnCrank();
+	gumballMachine1.insertCoin( 10 );
+        gumballMachine1.insertCoin( 10);
+        gumballMachine1.insertCoin( 25 );
+        gumballMachine1.insertCoin( 5 );
+	gumballMachine1.turnCrank();
+	 gumballMachine2.insertCoin( 25 );
+        gumballMachine2.insertCoin( 25 );
+	gumballMachine2.turnCrank();
+	gumballMachine.insertCoin( 5 );
+	gumballMachine.turnCrank();	
+	gumballMachine1.insertCoin( 25 );
+	gumballMachine1.turnCrank();	
+	gumballMachine2.insertCoin( 10 );
+	gumballMachine2.turnCrank();			
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-	}
+        
+    }
 }
